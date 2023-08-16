d20349e57cdd4b822304bee89a9b54f2157b7e05
Fatima <fawicoma@gmail.com>
2023-08-16 17:48:08 -0500

    Added Styles corrections, Form Completed page, Receipt functionallity

--- main.js
+++ main.js
@@ -15,3 +15,36 @@ const emailValidation = () => {
     validationMessage.innerHTML="Invalid email";
   }
 }
+
+// 
+const backbagAmount = document.getElementById("backbag-amount");
+const shoesAmount = document.getElementById("shoes-amount");
+const backbagSum = document.getElementById("backbag-sum");
+const backbagRest = document.getElementById("backbag-rest");
+const shoesSum = document.getElementById("shoes-sum");
+const shoesRest = document.getElementById("shoes-rest");
+
+let backbagCounter = 1;
+let shoesCounter = 1;
+
+backbagSum.addEventListener('click', () => {
+  backbagCounter++
+  backbagAmount.innerHTML = backbagCounter;
+})
+backbagRest.addEventListener('click', () => {
+  if(backbagCounter > 0){
+    backbagCounter--
+    backbagAmount.innerHTML = backbagCounter;
+  }
+})
+
+shoesSum.addEventListener('click', () => {
+  shoesCounter++
+  shoesAmount.innerHTML = shoesCounter;
+})
+shoesRest.addEventListener('click', () => {
+  if(shoesCounter > 0){
+    shoesCounter--
+    shoesAmount.innerHTML = shoesCounter;
+  }
+})
